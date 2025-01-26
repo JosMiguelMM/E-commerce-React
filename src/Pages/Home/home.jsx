4b456cad1b186b98c6d3596fbae74eaf02749b2f
JosMiguelMM <miguelmendez0802@gmail.com>
2025-01-26 01:09:31 -0500

    Cards

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,11 +1,24 @@
+import { useState, useEffect } from "react";
 import Card from "../../Components/Card/card";
 import Layout from "../../Components/Layout/Layout";
 
+
 export default function Home() {
+  const [productos, setProductos] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((response) => response.json())
+      .then((data) => setProductos(data));
+  }, []);
   return (
-    <Layout >
+    <Layout>
       Home
-      <Card />
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {productos?.map((producto, index) => (
+          <Card key={index} {...producto} />
+        ))}
+      </div>
     </Layout>
   );
 }
